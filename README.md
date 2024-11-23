299bcaef8e413796a16b589ddd91762a7cc0020a
Narazaka <info@narazaka.net>
2024-11-23 17:08:29 +0900

    impl

---
+++ README.md
@@ -0,0 +1,33 @@
+# Smart Collider
+
+Collider to ride when you want to ride
+
+乗りたいときに乗れるコライダー
+
+## Install
+
+### VRChat Creaters Companion (VCC)
+
+1. Press "Add to VCC" on https://vpm.narazaka.net/ to add Narazaka's repository to VCC.
+2. Make sure that "Narazaka VPM Listing" is enabled in VCC -> Settings -> Packages -> Installed Repositories.
+3. Install "Smart Collider" from your project's "Manage Project".
+
+---
+
+1. https://vpm.narazaka.net/ から「Add to VCC」ボタンを押してリポジトリをVCCにインストールします。
+2. VCCでSettings→Packages→Installed Repositoriesの一覧中で「Narazaka VPM Listing」にチェックが付いていることを確認します。
+3. 「Manage Project」から「Smart Collider」をインストールします。
+
+## Usage
+
+Place SmartCollider.prefab in the scene.
+Collider will be turned on when the player is above the bottom of the PlayerDetector.
+Adjust the size of the collider as you like.
+
+シーンにSmartCollider.prefabを置きます。
+PlayerDetectorの底面より上にプレイヤーが存在するときColliderがONになります。
+好きなようにコライダーのサイズを調整して下さい。
+
+## License
+
+[Zlib License](LICENSE.txt)
---
+++ Runtime/SmartCollider.cs
@@ -0,0 +1,62 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Narazaka.VRChat.SmartCollider
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    [RequireComponent(typeof(Collider))]
+    public class SmartCollider : UdonSharpBehaviour
+    {
+        [SerializeField] Collider[] TargetColliders;
+
+        float _y;
+        bool _active = true;
+
+        void OnEnable()
+        {
+            SetY();
+            SetCollidersActive(false);
+        }
+
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if (!player.isLocal) return;
+
+            SetY();
+            SetCollidersActive(player.GetPosition().y >= _y);
+        }
+
+        public override void OnPlayerTriggerStay(VRCPlayerApi player)
+        {
+            if (!player.isLocal) return;
+
+            SetCollidersActive(player.GetPosition().y >= _y);
+        }
+
+        public override void OnPlayerTriggerExit(VRCPlayerApi player)
+        {
+            if (!player.isLocal) return;
+
+            SetCollidersActive(false);
+        }
+
+        void SetCollidersActive(bool active)
+        {
+            if (_active == active) return;
+
+            _active = active;
+            var len = TargetColliders.Length;
+            for (var i = 0; i < len; i++)
+            {
+                TargetColliders[i].enabled = active;
+            }
+        }
+
+        void SetY()
+        {
+            _y = GetComponent<Collider>().bounds.min.y;
+        }
+    }
+}
